3825e3909e0f896a4d18720911daf3c92f49cb81
Ahmed Abdelmonem <131928792+Ahmedabdelmonem7@users.noreply.github.com>
2024-06-03 20:09:37 +0300

    Update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -256,11 +256,11 @@ document.querySelector('.lang').onchange = (e)=>{
     }
 }
 function arLang(){
-    window.location.replace(`http://${window.location.hostname}:${window.location.port}/public/index-ar.html`);
+    window.location.replace(`http://${window.location.hostname}:${window.location.port}/goldenngreen/public/index-ar.html`);
     window.localStorage.setItem('language', 'ar');
 }
 function enLang(){
-    window.location.replace(`http://${window.location.hostname}:${window.location.port}/index.html`);
+    window.location.replace(`http://${window.location.hostname}:${window.location.port}/goldenngreen/index.html`);
     window.localStorage.setItem('language', 'en');
 }
 
